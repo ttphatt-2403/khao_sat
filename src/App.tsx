7969c707b8db9b172f43c20fce6a592a1ea77aa5
ttphatt-2403 <ptt24032004@gmail.com>
2026-09-09 23:32:31 +0700

    fix: switch to HashRouter to fix blank screen in Figma/Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import SurveyPage from "./pages/SurveyPage";
 import { Dashboard } from "./pages/Dashboard";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<SurveyPage />} />
         <Route path="/mo-ni-dashboard-123" element={<Dashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
